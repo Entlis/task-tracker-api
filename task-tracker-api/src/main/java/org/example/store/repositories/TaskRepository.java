b02f38cbae7b02091749acb4f05acc94f026d225
Entlis <72922516+Entlis@users.noreply.github.com>
2024-10-02 15:50:08 +0300

    Delete task-tracker-api directory

--- task-tracker-api/src/main/java/org/example/store/repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.store.repositories;
-
-import org.example.store.entities.TaskEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
-}
